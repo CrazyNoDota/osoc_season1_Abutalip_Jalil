09ef676a69d0bfbd96f2c338900edea9f13f8583
CrazyNoDota <akdzhalil@gmail.com>
2024-06-01 23:01:11 +0500

    Still design of control unit

--- level_3/lab03/ControlUnit.sv
+++ level_3/lab03/ControlUnit.sv
@@ -16,14 +16,19 @@ module ControlUnit(
     output en_5,
     output en_6,
     output en_7,
-    output en_inst
+    output en_inst,
+    output logic done
 );
 
     typedef enum logic [1:0] {S0, S1, S2} state_t;
     state_t state, next_state;
 
     always@(posedge clk) begin
-        state <= (reset) ? S0 : next_state;
+        if (reset) begin
+            state <= S0;
+            done <= 0;
+        end
+        state <= next_state;
     end
 
     always@(*) begin
@@ -31,9 +36,33 @@ module ControlUnit(
                 S0: begin 
                     mux_sel = instruction[15:13];
                     en_s = 1;
+                    next_state = S1;
                     en_c = 0;
-                    en_r = 0;
-                    alu_sel = 4'b0;
+                end
+                S1: begin
+                    mux_sel = instruction[12:10];
+                    en_s = 0;
+                    en_c = 1;
+                    alu_sel = instruction[6:3];
+                    alu_mode = instruction[2];
+                    done = 0;
+                    next_state = S2;
+                end
+                S3: begin
+                    done = 1;
+                    next_state = S0;
+                    case(instruction[15:13]):
+                        
+                        4'd1: en_0 = 1;
+                        4'd2: en_1 = 1;
+                        4'd3: en_2 = 1;
+                        4'd4: en_3 = 1;
+                        4'd5: en_4 = 1;
+                        4'd6: en_5 = 1;
+                        4'd7: en_6 = 1;
+                        4'd8: en_7 = 1;
+
+                    endcase
                 end
 
     end
